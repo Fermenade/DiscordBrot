d575ba712aba0690f1484026dbc3f51ca91f70db
Fermenade <knitteltom2@gmail.com>
2026-03-17 09:53:06 +0100

    added horrible unit test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using BelegtesBrot;
 using BelegtesBrot.MinecraftServer;
+using Timer = System.Timers.Timer;
 
 namespace TestProject1;
 
@@ -13,12 +14,38 @@ public class Tests
     [Test]
     public async Task Test1()
     {
+        Assert.Pass();
+        return;
         Session session = new Session(1234);
         DirectoryInfo directoryInfo = new(session.BaseFolder.DirectoryInfo + "/MinecraftServer");
         directoryInfo.Create();
         MinecraftServer minecraftServer = new MinecraftServer(directoryInfo,session);
         minecraftServer.StartServer();
         await Task.Delay(-1);
-        Assert.Pass();
+    }
+
+    [Test]
+    public async Task Test2()
+    {
+        Timer timer = new()
+        {
+            Interval = 1000,
+            AutoReset = false,
+            Enabled = false,
+        };
+        timer.Elapsed += (sender, args) =>
+        {
+            Console.WriteLine(DateTime.Now);
+            Assert.Pass();
+        };
+        timer.Start();
+        Task.Delay(-1).Wait();
+        /*Session session = new Session(1234);
+        HallOfFame hallOfFame = new HallOfFame(session.BaseFolder.DirectoryInfo);
+        var e =hallOfFame.GetEntries();
+        foreach (var VARIABLE in e)
+        {
+            Console.WriteLine(VARIABLE);
+        }*/
     }
 }
